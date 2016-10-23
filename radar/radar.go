b96f0b48f8c24e48f0418d646c49b9b12473181b
Sergey Vasilenko <svasilenko@mirantis.com>
2016-10-23 13:00:24 +0300

    improve Radar logging

--- radar/radar.go
+++ radar/radar.go
@@ -1,6 +1,7 @@
 package radar
 
 import (
+	"fmt"
 	"gopkg.in/xenolog/go-tiny-logger.v1"
 	"net"
 	"strings"
@@ -20,18 +21,19 @@ func (r *Radar) Run(if_name string, passwd string) {
 		// addr  net.Addr
 		addrs []net.Addr
 	)
+	RR := fmt.Sprintf("Radar(%s):", if_name)
 	if r.nic, err = net.InterfaceByName(if_name); err != nil {
-		r.log.Warn("Radar: network interface '%s' not found.", if_name)
+		r.log.Warn("%s network interface not found.", RR)
 		return
 	}
 	if addrs, err = r.nic.Addrs(); err != nil || 1 > len(addrs) {
-		r.log.Warn("Radar: No ip addresses for interface '%s' given.", if_name)
+		r.log.Warn("%s No ip addresses given.", RR)
 		return
 	}
-	r.log.Debug("Radar: interface '%s' has %d addresses", if_name, len(addrs))
+	r.log.Debug("%s interface has %d addresses", RR, len(addrs))
 	for i, a := range addrs {
 		addr := a.String()
-		r.log.Debug("Radar: %02d: interface '%s' has address '%s'", i, if_name, addr)
+		r.log.Debug("%s %02d: interface has address '%s'", RR, i, addr)
 		// if r.ipv4only && strings.Contains(addr, ":") { // is it a prohibited ipv6 address
 		if strings.Contains(addr, ":") {
 			continue
@@ -41,10 +43,10 @@ func (r *Radar) Run(if_name string, passwd string) {
 		}
 	}
 	if len(r.ipv4) == 0 {
-		r.log.Warn("Radar: No IPv4 addresses for interface '%s' given.", if_name)
+		r.log.Warn("%s No IPv4 addresses given.", RR)
 		return
 	} else {
-		r.log.Debug("Radar: interface '%s' will use address '%s' while discovering", if_name, string(r.ipv4))
+		r.log.Info("%s address '%s' will be used while discovering", RR, string(r.ipv4))
 	}
 }
 
